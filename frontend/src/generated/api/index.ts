9c65275db4cebe5bcf94727f613a29528dc3b53c
Eno0516 <u898285h@alumni.osaka-u.ac.jp>
2025-10-13 22:39:02 +0900

    api追加

---
+++ frontend/src/generated/api/index.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export type { Item } from './models/Item';
+export type { LoginOrder } from './models/LoginOrder';
+export type { LoginUser } from './models/LoginUser';
+
+export { DefaultService } from './services/DefaultService';
---
+++ frontend/src/generated/api/services/DefaultService.ts
@@ -0,0 +1,39 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { Item } from '../models/Item';
+import type { LoginOrder } from '../models/LoginOrder';
+import type { LoginUser } from '../models/LoginUser';
+import type { CancelablePromise } from '../core/CancelablePromise';
+import { OpenAPI } from '../core/OpenAPI';
+import { request as __request } from '../core/request';
+export class DefaultService {
+    /**
+     * Order to login
+     * @param requestBody
+     * @returns LoginUser OK
+     * @throws ApiError
+     */
+    public static postLogin(
+        requestBody: LoginOrder,
+    ): CancelablePromise<LoginUser> {
+        return __request(OpenAPI, {
+            method: 'POST',
+            url: '/login',
+            body: requestBody,
+            mediaType: 'application/json',
+        });
+    }
+    /**
+     * Get all users
+     * @returns Item OK
+     * @throws ApiError
+     */
+    public static getManageplant(): CancelablePromise<Array<Item>> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/manageplant',
+        });
+    }
+}
