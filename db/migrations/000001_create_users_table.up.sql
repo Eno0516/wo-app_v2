7df16c4560848227540b78d67f6ff22f9be175fc
Eno0516 <u898285h@alumni.osaka-u.ac.jp>
2025-10-25 19:37:15 +0900

    マイグレーション追加

---
+++ db/migrations/000001_create_users_table.up.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,                  -- 内部用の連番ID
+    uuid UUID NOT NULL DEFAULT uuid_generate_v4(), -- 内部用の一意なUUID
+    username VARCHAR(50) UNIQUE NOT NULL,   -- ユーザー名（ユニーク制約）
+    password_hash VARCHAR(255) NOT NULL,    -- ハッシュ化したパスワード
+    created_at TIMESTAMP DEFAULT NOW(),     -- 作成日時
+    updated_at TIMESTAMP DEFAULT NOW()      -- 更新日時
+);
